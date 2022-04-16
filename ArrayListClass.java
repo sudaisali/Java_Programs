3b769fa986e72218cd1008e63159a1e143320593
Sudais Bin Sohail <sudaisali420@gmail.com>
2022-04-16 15:32:01 -0700

    one

---
+++ ArrayListClass.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class ArrayListClass {
+    ArrayList<Integer> list = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+    int size;
+    int index;
+
+
+    public void add(){
+
+        System.out.println("How many number you want to add");
+        size=sc.nextInt();
+        for (int i=0 ; i<size+1 ; i++){
+            list.add(sc.nextInt());
+        }
+    }
+    public void delete(){
+        System.out.println("Enter index number to delete");
+        index = sc.nextInt();
+        list.remove(index);
+        System.out.println(list);
+    }
+    public void sort(){
+        Collections.sort(list);
+        System.out.println("Sorted Array");
+        System.out.println(list);
+    }
+
+}
---
+++ Array_List_1.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Array_List_1 {
+    public static void main(String[] args) {
+        // DECLARE ARRAY LIST
+        ArrayList<String> names = new ArrayList<String>();
+        // ADD ELEMENT IN LIST
+        names.add("SUDAIS ALI");
+        names.add("Burhan ali saif");
+        names.add("Talha Saddique");
+        names.add("Usama Naseer");
+        names.add("Abdullah saif");
+        names.add("Abdullah ashiq");
+        System.out.print(names+"\n");
+        //REPLACE ELEMENT
+        names.set(1,"SUDAIS BIN SOHAIL");
+        names.set(0,"Burhan Ali Saif");
+        System.out.print(names+"\n");
+        // ADD ELEMENT IN PARTICULAR INDEX
+        names.add(0,"Abdullah Saif");
+        System.out.println(names+"\n");
+        names.remove(5);
+        // PRINT ARRAY LIST BY LOOP
+        System.out.println(names);
+        for (int i=0 ; i<names.size();i++){
+            System.out.println(i+"\t"+names.get(i));
+        }
+        //SORT ELEMENT WITH COLLECTION
+        Collections.sort(names);
+        System.out.println(names);
+
+    }
+}
