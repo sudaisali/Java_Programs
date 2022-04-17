0f4ce65c3a4a14ac3517f95a533d66c98bdce7b7
Sudais Bin Sohail <sudaisali420@gmail.com>
2022-04-16 17:35:35 -0700

    Factorial Program

---
+++ programs/factorial.java
@@ -0,0 +1,19 @@
+package programs;
+
+import java.util.Scanner;
+
+public class factorial {
+
+    public static void main(String[] args) {
+        int number;
+        int fac=1;
+
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter number to find factorial");
+        number = obj.nextInt();
+        for ( int i=1 ; i<=number ; i++){
+           fac = fac*i;
+        }
+        System.out.println(fac);
+    }
+}
